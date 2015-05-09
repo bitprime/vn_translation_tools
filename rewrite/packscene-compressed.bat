8f38bc68f468e0bed8efe0eab6deaa5d897e3b82
bitprime <kubayubay@outlook.com>
2015-05-08 21:15:06 -0700

    Little tweaks to batch scripts to use v2 utils

--- rewrite/packscene-compressed.bat
+++ rewrite/packscene-compressed.bat
@@ -1,9 +1,12 @@
 @echo off
 mkdir Scene\SceneNew  >nul 2>&1
-for %%s in (Scene\\*.ss) do ssinsert.exe "%%s" "Scene\%%~ns-str.txt" "Scene\SceneNew\%%~nxs"
+for %%s in (Scene\\*.ss) do ssinsert-v200.exe "%%s" "Scene\%%~ns-str.txt" "Scene\SceneNew\%%~nxs"
 copy /Y Scene\vars.dat Scene\SceneNew\vars.dat >nul 2>&1
+copy /Y Scene\vars1.dat Scene\SceneNew\vars1.dat >nul 2>&1
+copy /Y Scene\vars2.dat Scene\SceneNew\vars2.dat >nul 2>&1
+copy /Y Scene\vars3.dat Scene\SceneNew\vars3.dat >nul 2>&1
 @echo Finished inserting scripts
-createpck.exe "Scene\SceneNew" "Scene.pck" -c
+createpck.exe "Scene\SceneNew" "Scene.pck" -c -e
 @echo on
 @echo Finished packing new Scene.pck
 pause
--- rewrite/unpackscene.bat
+++ rewrite/unpackscene.bat
@@ -1,7 +1,7 @@
 @echo off
 parsepck.exe Scene.pck Scene
 @echo Finished extracting Scene.pck
-for %%s in (Scene\\*.ss) do ssdump.exe "%%s" "Scene\%%~ns-str.txt"
+for %%s in (Scene\\*.ss) do ssdump-v200.exe "%%s" "Scene\%%~ns-str.txt"
 @echo Finished dumping scripts
 @echo on
 pause
